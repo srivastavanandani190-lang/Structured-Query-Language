c7b94fec5cce91c9921cc9fb540ca7adeee77823
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-07-01 15:27:59 +0530

    sql creating table

--- day1.sql
+++ day1.sql
@@ -1,8 +1,7 @@
-CREATE DATABASE startersql; --creates a new database named startersql
+CREATE DATABASE startersql; -- creates a new database named startersql
+USE startersql; -- selects the database to use
 
-USE startersql; --selects the database to use
-
---creates a new table named users
+-- creates a new table named users
 CREATE TABLE users( 
 id INT AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(100) NOT NULL,
@@ -11,10 +10,26 @@ gender ENUM('Male','Female','Other'),
 date_of_birth DATE,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
---displays all the data in the users table
+-- displays all the data in the users table
 SELECT * FROM users;
 
---deletes the database and all its contents
+-- deletes the database and all its contents
 DROP DATABASE startersql;
 
+-- Selects the email and gender
+SELECT email,gender from users;
+
+-- renames the table users to programmers
+RENAME TABLE users to programmers;
+
+-- adds a new column named is_active to the programmers table
+ALTER TABLE programmers ADD column is_active BOOLEAN DEFAULT true;
+SELECT * FROM programmers;
+
+-- drops the is_active column from the programmers table
+ALTER TABLE programmers DROP COLUMN is_active;
+SELECT * FROM programmers;
+
+ALTER TABLE programmers MODIFY COLUMN name VARCHAR(150);
+ALTER TABLE programmers MODIFY COLUMN date_of_birth DATETIME FIRST;
+SELECT * FROM programmers;
