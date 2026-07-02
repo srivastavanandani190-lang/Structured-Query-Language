9f39ad4fb8bdda0b5ecafbc499565a72e85c57f4
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-07-02 13:03:10 +0530

    updation,selection and deletion

---
+++ QUERY/day2.sql
@@ -0,0 +1,74 @@
+CREATE DATABASE IF NOT EXISTS startersql;
+USE startersql;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    gender ENUM('Male', 'Female', 'Other'),
+    date_of_birth DATE,
+    salary DECIMAL(10, 2),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+USE startersql;
+INSERT INTO users (name, email, gender, date_of_birth, salary) VALUES
+('Aarav', 'aarav@example.com', 'Male', '1995-05-14', 65000.00),
+('Ananya', 'ananya@example.com', 'Female', '1990-11-23', 72000.00),
+('Raj', 'raj@example.com', 'Male', '1988-02-17', 58000.00),
+('Sneha', 'sneha@example.com', 'Female', '2000-08-09', 50000.00),
+('Farhan', 'farhan@example.com', 'Male', '1993-12-30', 61000.00),
+('Priyanka', 'priyanka@example.com', 'Female', '1985-07-12', 84000.00),
+('Aisha', 'aisha@example.com', 'Female', '1997-03-25', 56000.00),
+('Aditya', 'aditya@example.com', 'Male', '1992-06-17', 69000.00),
+('Meera', 'meera@example.com', 'Female', '1989-09-05', 77000.00),
+('Ishaan', 'ishaan@example.com', 'Male', '2001-10-02', 45000.00),
+('Tanvi', 'tanvi@example.com', 'Female', '1994-04-18', 62000.00),
+('Rohan', 'rohan@example.com', 'Male', '1986-12-01', 75000.00),
+('Zoya', 'zoya@example.com', 'Female', '1998-01-15', 54000.00),
+('Karan', 'karan@example.com', 'Male', '1990-08-22', 68000.00),
+('Nikita', 'nikita@example.com', 'Female', '1987-03-10', 71000.00),
+('Manav', 'manav@example.com', 'Male', '1996-11-29', 61000.00),
+('Divya', 'divya@example.com', 'Female', '1991-02-28', 57000.00),
+('Harshit', 'harshit@example.com', 'Male', '1993-09-09', 65000.00),
+('Ritika', 'ritika@example.com', 'Female', '1999-05-05', 52000.00),
+('Imran', 'imran@example.com', 'Male', '1995-07-30', 63000.00),
+('Juhi', 'juhi@example.com', 'Female', '1992-10-14', 59000.00),
+('Tushar', 'tushar@example.com', 'Male', '1990-01-08', 73000.00),
+('Lata', 'lata@example.com', 'Female', '1984-11-11', 78000.00),
+('Yash', 'yash@example.com', 'Male', '1997-06-06', 64000.00),
+('Fatima', 'fatima@example.com', 'Female', '1993-03-03', 55000.00);
+SELECT * FROM users;
+SELECT * FROM users WHERE gender<>'Male'; -- not equal i.e female
+SELECT * FROM users WHERE id<=10;
+SELECT * FROM users WHERE date_of_birth IS NOT NULL;
+SELECT * FROM users WHERE date_of_birth BETWEEN '1990-09-09' AND '1999-09-09';
+SELECT * FROM users WHERE gender in ('Male');
+SELECT * FROM users WHERE gender='Female' AND salary>'70000' LIMIT 5;
+SELECT * FROM users WHERE gender='Female' OR salary>'70000' ORDER BY date_of_birth ASC; -- DESC
+
+-- update is used to modify existing records in a table. 
+-- The SET clause specifies the column(s) to be updated and their new values.
+-- The WHERE clause is used to specify which records should be updated. 
+-- If the WHERE clause is omitted, all records in the table will be updated.
+UPDATE users SET salary=80000 WHERE id=1;
+UPDATE users SET salary=salary+10000 WHERE salary<60000;
+
+-- delete is used to remove records from a table.
+DELETE FROM users WHERE salary<65000;
+
+SELECT * FROM users;
+DROP TABLE users;
+-- always use WHERE unless you are intentionally updating/deleting everything in the table.
+--  If you omit the WHERE clause, all records will be affected.
+
+
+
+
+
+
+
+
+
+
+
+
